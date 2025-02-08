c940d6ac63777b893bff2f477a17f386b7f661db
Aashika Lilaramani <aashika.lilaramani@gmail.com>
2025-02-08 13:58:14 -0600

    started google maps integration

--- src/main.js
+++ src/main.js
@@ -3,4 +3,14 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 
+import VueGoogleMaps from '@fawmi/vue-google-maps'
+
+const app = createApp(App);
+app.use(VueGoogleMaps, {
+  load: {
+    key: 'AIzaSyCxcxrC_6zyUokJ4ewVal-3m_DduyBzWGw',
+  },
+}).mount('#app');
+
 createApp(App).mount('#app')
+ 
